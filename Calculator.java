096e87f40a4b1d1085bd7b0e79c010c7dec2e157
Lalit Shinde <102148574+Lalitshinde18@users.noreply.github.com>
2025-02-05 08:44:45 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package Challenges_Solving_Problems84;
+
+public class Calculator {
+
+    public int add(int first , int second){
+        return first + second;
+
+    }
+    public int add(int first , int second , int third){
+        return first + second + third;
+    }
+
+    public  double add(double first , double second ){
+        return  first + second;
+    }
+
+    public static void main(String[] args) {
+        Calculator cl = new Calculator();
+        System.out.println(cl.add(20,20));
+        System.out.println(cl.add(20.2,20.1));
+        System.out.println(cl.add(20,20,10));
+    }
+
+}
