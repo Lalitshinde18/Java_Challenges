956b935a9e3570976e498994a7930856065fcdfb
Lalit Shinde <102148574+Lalitshinde18@users.noreply.github.com>
2025-02-21 13:33:53 +0530

    Add files via upload

---
+++ inheritance_of_java.java
@@ -0,0 +1,32 @@
+
+
+
+     class base {
+
+       base (){  // you can used constructor
+           System.out.println("this is a constructor");
+       }
+
+         public int x;
+         public int getX() {
+             return x;
+         }
+
+         public void setX(int x) {
+             this.x = x;
+         }
+
+
+
+
+     }
+     public class inheritance_of_java {
+    public static void main(String[] args) {
+
+        base b =new base();
+
+
+
+
+    }
+}
