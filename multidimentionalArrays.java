b6ddf5fa61c0881aa18e1de861b323e9c84ebfde
Lalit Shinde <102148574+Lalitshinde18@users.noreply.github.com>
2025-02-22 22:42:44 +0530

    Add files via upload

---
+++ multidimentionalArrays.java
@@ -0,0 +1,11 @@
+public class multidimentionalArrays {
+    public static void main(String[] args) {
+        int [][] myNumber = {{1,3,4},{3,4,6}};
+        for (int i=0; i<myNumber.length;i++){
+            for(int j=0; j<myNumber[i].length;j++){
+           System.out.println(myNumber[i][j]);
+            }
+            
+        }
+    }
+}
