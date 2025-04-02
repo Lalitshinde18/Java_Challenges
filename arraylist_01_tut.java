5215a510cbbe5dcc934e994a012b893f866a614d
Lalit Shinde <102148574+Lalitshinde18@users.noreply.github.com>
2025-04-02 18:33:36 +0530

    Add files via upload

---
+++ arraylist_01_tut.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class arraylist_01_tut {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<String> cars =new ArrayList<String>();
+
+        cars.add("volvo");
+        cars.add("mahendra");
+        cars.add("hero");
+        cars.add("volvo");
+        cars.add("mahendra");
+        cars.add("hero");
+
+        System.out.println(cars);
+    }
+}
