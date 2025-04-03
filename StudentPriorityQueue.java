e94a2a3bd4df00196c480eebd895834adfaab0e6
Lalit Shinde <102148574+Lalitshinde18@users.noreply.github.com>
2025-04-03 10:22:39 +0530

    Add files via upload

--- StudentPriorityQueue.java
+++ StudentPriorityQueue.java
@@ -1,31 +1,54 @@
-package Collection_Framework.Challenges;
+package Collection_Framework.Challenges.CollectionClassSp;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class StudentPriorityQueue {
+/*
+Create a PriorityQueue of a custom class Student with attributes
+name and grade . use a comparator to order by grade
+ */
 
     public static void main(String[] args) {
-        PriorityQueue<Student> queue = new PriorityQueue<>(new Comparator<Student>() {
+
+        PriorityQueue<student> queue = new PriorityQueue<>(new Comparator<student>() {
             @Override
-            public int compare(Student student, Student t1) {
-                return student.getGrade() - t1.getGrade();
+            public int compare(student student, student t1) {
+                return student.getGrade() - t1.getGrade() ;
             }
         });
-        queue.offer(new Student("lalit",'A'));
-        queue.offer(new Student("Gaurav",'b'));
-        queue.offer(new Student("ramesh",'c'));
+
+        queue.offer(new student("Lalit",'A'));
+        queue.offer(new student("Rohan",'B'));
+        queue.offer(new student("Mahesh",'C'));
+        queue.offer(new student("sakshi",'F'));
+        queue.offer(new student("Gauri",'D'));
         System.out.printf("Queue is: %s",queue);
+        System.out.printf("Queue is: %s\n",queue.poll());
+        System.out.printf("Queue is: %s\n",queue.poll());
+        System.out.printf("Queue is: %s\n",queue.poll());
+        System.out.printf("Queue is: %s\n",queue.poll());
+        System.out.printf("Queue is: %s\n",queue.poll());
+
+
+
 
 
     }
 
-    private  static class Student {
 
-        private final String name;
-        private final char grade;
 
-        public Student(String name, char grade) {
+
+
+    private static class student {
+
+
+
+        private final  String name;
+
+        private final   char grade;
+
+        public student(String name, char grade) {
             this.name = name;
             this.grade = grade;
         }
@@ -40,13 +63,8 @@ public class StudentPriorityQueue {
 
         @Override
         public String toString() {
-            return "Student{" +
-                    "name='" + name + '\'' +
-                    ", grade=" + grade +
-                    '}';
+            return name + ": " + grade;
         }
     }
-
-
 
 }
